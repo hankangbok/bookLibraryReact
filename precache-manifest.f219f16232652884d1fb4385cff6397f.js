88443f9d81336ae33e26b25e2fe9772907911b7f
Hannah Kang <kyouyatamax@gmail.com>
2018-11-27 14:46:46 -0800

    Updates

---
+++ precache-manifest.f219f16232652884d1fb4385cff6397f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5578990fd7715debd82d",
+    "url": "/bookLibraryReact/static/js/runtime~main.5578990f.js"
+  },
+  {
+    "revision": "00336bd881090d8d3a00",
+    "url": "/bookLibraryReact/static/js/main.00336bd8.chunk.js"
+  },
+  {
+    "revision": "0eec96f4ccba5611a9c2",
+    "url": "/bookLibraryReact/static/js/1.0eec96f4.chunk.js"
+  },
+  {
+    "revision": "00336bd881090d8d3a00",
+    "url": "/bookLibraryReact/static/css/main.d8de1c5c.chunk.css"
+  },
+  {
+    "revision": "416bfdf6564147d39b671ea5ebc8c589",
+    "url": "/bookLibraryReact/index.html"
+  }
+];
