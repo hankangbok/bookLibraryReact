2616dc44733ab888d2072f68102d20693dd13128
Hannah Kang <kyouyatamax@gmail.com>
2019-01-08 17:36:07 -0800

    ensured that form would return user feedback at least

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import {NewBookFormEntry} from './newBookForm'
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import { NewBookFormEntry } from "./newBookForm";
 
-// dont touch this for now. 
+// dont touch this for now.
 // I'll set up form entry once the library display is good.
 // class AddNewBookForm extends React.Component {
 //   render() {
@@ -14,35 +14,36 @@ import {NewBookFormEntry} from './newBookForm'
 //   }
 // }
 
-class AddNewBookForm extends React.Component {
-  render() {
-    return <NewBookFormEntry />;
-  }
-}
+// class AddNewBookForm extends React.Component {
+//   render() {
+//     return <NewBookFormEntry />;
+//   }
+// }
 
 // this component displays the 'read' toggle button
 class ReadToggle extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      isReadorNot: this.props.thisBook.isRead,
-    }
+      isReadorNot: this.props.thisBook.isRead
+    };
     this.handleClick = this.handleClick.bind(this);
   }
-  
+
   handleClick() {
-    let newReadStatus = (this.state.isReadorNot === true) ? false : true;
+    let newReadStatus = this.state.isReadorNot === true ? false : true;
     this.setState({ isReadorNot: newReadStatus });
   }
   render() {
     let isReadButton;
-    if (this.state.isReadorNot===true) {
+    if (this.state.isReadorNot === true) {
       isReadButton = "Finished!";
     } else {
       isReadButton = "Working on It";
     }
     return (
-      <h3>Have you finished the book?
+      <h3>
+        Have you finished the book?
         <button onClick={this.handleClick}>
           {/* {this.state.isReadorNot.toString()} */}
           {isReadButton}
@@ -54,12 +55,15 @@ class ReadToggle extends React.Component {
 
 class RemoveBookFromLibrary extends React.Component {
   render() {
-    return <button title={this.props.title} onClick={this.props.onClick}>Remove this Book from Library</button>;
+    return (
+      <button title={this.props.title} onClick={this.props.onClick}>
+        Remove this Book from Library
+      </button>
+    );
   }
 }
 
 class SingleBook extends React.Component {
-
   render() {
     const aBook = this.props.aBook;
     const title = aBook.title;
@@ -73,78 +77,181 @@ class SingleBook extends React.Component {
         <h2>Pages: {numPages}</h2>
         <h2>Genre: {genre}</h2>
         <ReadToggle thisBook={aBook} />
-        <br></br>
+        <br />
         <RemoveBookFromLibrary title={title} onClick={this.props.onClick} />
       </div>
     );
-
   }
 }
 
 class LibraryContainer extends React.Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
-      allBooks:  [
-        { title: 'Lord of the Rings: Fellowship of the Ring', author: 'JRR Tolkien ', numPages: '1235', isRead: true, genre: 'Fantasy' },
-        { title: 'Lord of the Rings: The Two Towers', author: 'JRR Tolkien ', numPages: '1235', isRead: false, genre: 'Fantasy' },
-        { title: 'Lord of the Rings: Return of the King', author: 'JRR Tolkien ', numPages: '1235', isRead: false, genre: 'Fantasy' },
-        { title: 'Game of Thrones', author: 'George R R Martin ', numPages: '1235', isRead: true, genre: 'Fantasy' },
-        { title: 'Cell ', author: 'Stephen King ', numPages: '1235', isRead: false, genre: 'horror' },
-        { title: 'Design It Yourself', author: 'Ellen Lipton', numPages: '1235', isRead: false, genre: 'DIY' },
-        { title: 'Fullmetal Alchemist', author: 'Hiromu Arakawa', numPages: '1235', isRead: false, genre: 'manga' }
+      allBooks: [
+        {
+          title: "Lord of the Rings: Fellowship of the Ring",
+          author: "JRR Tolkien ",
+          numPages: "1235",
+          isRead: true,
+          genre: "Fantasy"
+        },
+        {
+          title: "Lord of the Rings: The Two Towers",
+          author: "JRR Tolkien ",
+          numPages: "1235",
+          isRead: false,
+          genre: "Fantasy"
+        },
+        {
+          title: "Lord of the Rings: Return of the King",
+          author: "JRR Tolkien ",
+          numPages: "1235",
+          isRead: false,
+          genre: "Fantasy"
+        },
+        {
+          title: "Game of Thrones",
+          author: "George R R Martin ",
+          numPages: "1235",
+          isRead: true,
+          genre: "Fantasy"
+        },
+        {
+          title: "Cell ",
+          author: "Stephen King ",
+          numPages: "1235",
+          isRead: false,
+          genre: "horror"
+        },
+        {
+          title: "Design It Yourself",
+          author: "Ellen Lipton",
+          numPages: "1235",
+          isRead: false,
+          genre: "DIY"
+        },
+        {
+          title: "Fullmetal Alchemist",
+          author: "Hiromu Arakawa",
+          numPages: "1235",
+          isRead: false,
+          genre: "manga"
+        }
       ]
-    }
-    this.handleRemoveClick=this.handleRemoveClick.bind(this);
+    };
+    this.handleRemoveClick = this.handleRemoveClick.bind(this);
   }
-  
+
   // Even handler for removing books
-  handleRemoveClick(e){ 
+  handleRemoveClick(e) {
     const theTitle = e.currentTarget.title;
     console.log("this is the book to remove" + theTitle);
 
     const allBooks1 = this.state.allBooks;
     const index = allBooks1.findIndex(eachBook => eachBook.title === theTitle);
-    allBooks1.splice(index,1);
-    this.setState({allBooks: allBooks1});
+    allBooks1.splice(index, 1);
+    this.setState({ allBooks: allBooks1 });
   }
 
-  
   render() {
     const rows = [];
-    this.state.allBooks.forEach((bookinlist) => {
+    this.state.allBooks.forEach(bookinlist => {
       rows.push(
         <SingleBook
           aBook={bookinlist}
           title={bookinlist.title}
           key={bookinlist.title}
-          onClick={this.handleRemoveClick} />
+          onClick={this.handleRemoveClick}
+        />
       );
-
     });
 
-    return (
-      <div id="librarycontainer2">{rows}</div>
-    );
+    return <div id="librarycontainer2">{rows}</div>;
   }
 }
 
 
-
 class Library extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      dummystate: "blank",
+      newBook: { title: "Your new book will go here", key: "placeholder" },
+      formValidated: 'false'
+    };
+    this.handleFormSubmit = this.handleFormSubmit.bind(this);
+  }
+  handleFormSubmit(e) {
+    e.preventDefault();
+    const allinputs = document.querySelectorAll("input[required]");
+    // console.log(allinputs);
+    var formcompleted = true;
+    var buttonscompleted = false;
+    allinputs.forEach((inputitem, index) => {
+      // console.log(inputitem.value);
+      if (inputitem.value === null || inputitem.value == "") {
+        formcompleted = false;
+      }
+    });
+    const radioinputs = document.getElementsByName("haveRead");
+    radioinputs.forEach(onebutton => {
+      if (onebutton.checked) {
+        buttonscompleted = true;
+      }
+    });
+    if (formcompleted && buttonscompleted) {
+      const title = document.getElementById("title").value;
+      const author = document.getElementById("authorname").value;
+      const pages = document.getElementById("pages").value;
+      const genre = document.getElementById("genre").value;
+      const haveRead = document.querySelector('input[name="haveRead"]:checked')
+        .value;
+      console.log(title);
+      // console.log(author);
+      // console.log(pages);
+      // console.log(genre);
+      // console.log(haveRead);
+      const newBook = {
+        title: title,
+        author: author,
+        numPages: pages,
+        genre: genre,
+        isRead: haveRead
+      };
+      console.log(this.state.formValidated);
+      this.setState = ({ formValidated: 'true' },
+      function() {
+        console.log(this.state.formValidated);
+      });
+      this.setState = { newBook: newBook };
+      console.log(this.state.newBook);
+      const asRead = haveRead ? 'read' : 'not read' ;
+      alert(`Your book would have been added with 
+      Title: ${title}, 
+      Author: ${author}, 
+      Pages: ${pages}, 
+      Genre: ${genre} 
+      and marked as ${asRead} `);
+      // console.log(newBook);
+    } else {
+      alert("Please fill out all fields");
+    }
+
+  }
   render() {
     return (
       <div>
-        <NewBookFormEntry onClick={this.props.handleFormClick} />
-        <AddNewBookForm />
+        <NewBookFormEntry handleSubmit={this.handleFormSubmit} />
+        {/* <SingleBook aBook={this.state.newBook} /> */}
         <LibraryContainer books={this.props.booksList} />
-        <AddNewBookForm />
+        {/* <AddNewBookForm /> */}
       </div>
     );
   }
 }
 
-// This is a list of 'book' objects 
+// This is a list of 'book' objects
 // This version of the library project does not have backend data
 // const BOOKSLIST = [
 //   { title: 'book1', author: 'autho1 ', numPages: '1235', isRead: false, genre: 'scifi' },
@@ -155,7 +262,6 @@ class Library extends React.Component {
 //   { title: 'book6', author: 'autho6 ', numPages: '1235', isRead: false, genre: 'scifi' },
 //   { title: 'book7', author: 'autho8 ', numPages: '1235', isRead: false, genre: 'scifi' }
 // ];
-
 
 ReactDOM.render(
   <Library />,
--- src/newBookForm.js
+++ src/newBookForm.js
@@ -1,66 +1,138 @@
-import React from 'react';
+import React from "react";
+
+export class NewBookFromForm extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>This is where the new Book object goes</h1>
+      </div>
+    );
+  }
+}
 
 export class NewBookFormEntry extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      isVisible:false,
-    }
+      isVisible: true,
+      // visibility: "visible"
+      formValidated: false,
+    };
+    // this.handleFormSubmit = this.handleFormSubmit.bind(this);
     this.handleFormClick = this.handleFormClick.bind(this);
   }
+
+// Should get the values of fields in the form and save them to a new book object
+  // handleFormSubmit(e) {
+  //   e.preventDefault();
+  //   const allinputs = document.querySelectorAll('input[required]');
+  //   // console.log(allinputs);
+  //   var formcompleted = true;
+  //   var buttonscompleted = false;
+  //   allinputs.forEach((inputitem, index) => {
+  //     // console.log(inputitem.value);
+  //     if (inputitem.value===null || inputitem.value=='') {
+  //       formcompleted = false;
+  //     }
+  //   })
+  //   const radioinputs = document.getElementsByName('haveRead');
+  //   radioinputs.forEach((onebutton) => {
+  //     if (onebutton.checked) {
+  //       buttonscompleted = true;
+  //     }
+  //   })
+  //   if (formcompleted && buttonscompleted) {
+  //     const title = document.getElementById('title').value;
+  //     const author = document.getElementById('authorname').value;
+  //     const pages = document.getElementById('pages').value;
+  //     const genre = document.getElementById('genre').value;
+  //     const haveRead = document.querySelector('input[name="haveRead"]:checked').value;
+  //     // console.log(title);
+  //     // console.log(author);
+  //     // console.log(pages);
+  //     // console.log(genre);
+  //     // console.log(haveRead);
+  //     const newBook = {
+  //       title: title,
+  //       author: author,
+  //       numPages: pages,
+  //       genre: genre,
+  //       isRead:haveRead
+  //     };
+  //     this.setState=({formValidated:true, newBook:newBook});
+  //     return newBook;
+  //     // console.log(newBook);
+  //   } else {
+  //     alert('Please fill out all fields');
+  //   }
+  // }  
 
   handleFormClick() {
     console.log("something was clicked");
     console.log(this.state.isVisible);
-    this.setState=( {isVisible: true}, () => {
-      console.log(this.state.isVisible);
-    });   
+    // this.setState=( {isVisible: true}, () => {
+    //   console.log(this.state.isVisible);
+    // });
+    // this.setState = ({visibility:'visible'});
+    this.setState = ({ isVisible: true });
+    // console.log(this.state.isVisible);
+    
   }
+
   render() {
-    if (this.state.isVisible) {
+    if (this.state.isVisible===true) {
       return (
-        <form id="addBookForm">
+        <form id="addBookForm" visibility='visible' completed='false'>
+          <h1>Want to Add a New Book?</h1>
+          <h2>Sorry, new book addition is not available for now.</h2>
+          <h2>This is a front-end only webpage for now.</h2>
           <p>
-            <label for="title"> What is the title of the book?</label>
-            <input id="title" name="booktile" required />
+            <label htmlFor="title"> What is the title of the book?</label>
+            <input id="title" name="booktitle" required />
           </p>
           <p>
-            <label for="author"> Who is the author of the book?</label>
+            <label htmlFor="author"> Who is the author of the book?</label>
             <input id="authorname" name="authorname" required />
           </p>
           <p>
-            <label for="pages"> How many pages does the book have?</label>
+            <label htmlFor="pages"> How many pages does the book have?</label>
             <input id="pages" name="pages" required />
           </p>
           <p>
-            <label for="genre">What genre is the book?</label>
+            <label htmlFor="genre">What genre is the book?</label>
             <input id="genre" name="genre" required />
           </p>
+      {/*TODO: known crash case. if you submit without clicking radio button.  */}
           <p>
             Have you finished this book?
-            <label for="haveRead"> Yes</label>
-            <input type="radio" name="haveRead" />
-            <label for="haventRead">Nope</label>
-            <input type="radio" name="haveRead" />
+            <label htmlFor="haveRead"> Yes</label>
+            <input type="radio" name="haveRead" value = "true" />
+            <label htmlFor="haventRead">Nope</label>
+            <input id ='haveRead' type="radio" name="haveRead" value = "false" />
           </p>
-          <button>Submit</button>
+          {/* <button onClick= {this.handleFormSubmit}>Submit</button> */}
+          <button onClick = {this.props.handleSubmit} >Submit (disabled) </button>
         </form>
-
       );
     } else {
-      console.log("This is the current state" +this.state.isVisible);
+      console.log("This is the current state " + this.state.isVisible);
       return (
-        <button id="addBookForm" onClick={this.handleFormClick}> Add a New Book~~!</button>
+        <div>
+          <button id="addBookForm" onClick={this.handleFormClick}>
+          Add a New Book~~!
+          </button>
+          {/* <form id="addBookForm" visibility='hidden'></form> */}
+        </div>
       );
     }
   }
 }
 
 // function addBooktoLibrary() {
-//   var newBook=[ 
-//     title: 'book1', 
-//     author: 'autho1 ', 
-//     numPages: '1235', 
-//     isRead: false, 
+//   var newBook=[
+//     title: 'book1',
+//     author: 'autho1 ',
+//     numPages: '1235',
+//     isRead: false,
 //     genre: 'scifi' }
 //   ];
